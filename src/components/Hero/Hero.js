3c5df70fb5c9057e627c68fd25004d5de95c6cea
Seweryn Golba <sgoolb@gmail.com>
2023-08-10 13:41:32 +0200

    video fix

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -4,13 +4,9 @@ import "./Hero.css";
 const Hero = () => {
   return (
     <div className="hero">
-      <div className="videoBg">
-        <video autoPlay loop muted playsInline>
-          <source src={video} type="video/mp4" />
-        </video>
-      </div>
+      <video src={video} autoPlay loop muted />
       <div className="heroContent">
-        <h1>DISCOVER THE BEST FILMS ONLINE</h1>
+        <h1 className="title">DISCOVER THE BEST FILMS ONLINE</h1>
       </div>
     </div>
   );
